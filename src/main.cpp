7af1d480d00c5c4ff7def892479b231d1006be32
elcioamorim <elcioamorim12@gmail.com>
2024-03-23 15:41:37 -0300

    ler matriz do documento

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cstdlib> // Para a função exit
 
 using namespace std;
 
-ifstream abrir_arquivo(){ 
-        ifstream arq_input;
-
-        arq_input.open("input.mps");
-        if (!arq_input.is_open()) {
-            cout << "Ocorreu um erro ao abrir o arquivo.\n";
-        }
-       int linhas_colunas = 0;
-
-        arq_input >> linhas_colunas;
-
-        if (linhas_colunas < 5 ) {
-            cout << "A matriz não é maior ou igual ou 5x5";
-            arq_input.close();
-        }
-
+ifstream abrir_arquivo() {
+    ifstream arq_input;
+    arq_input.open("input.mps");
+    if (!arq_input.is_open()) {
+        cout << "Ocorreu um erro ao abrir o arquivo.\n";
+        exit(1);
+    }
     return arq_input;
 }
 
+vector<vector<int>> ler_arquivo(ifstream& arq_input) {
+    int tamanho;
+    arq_input >> tamanho;
+
+    vector<vector<int>> matriz_inicial(tamanho, vector<int>(tamanho));
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            arq_input >> matriz_inicial[i][j];
+        }
+    }
+    return matriz_inicial;
+}
+
+void imprimir_matriz(const vector<vector<int>>& matriz) {
+    for (const auto& linha : matriz) {
+        for (int elemento : linha) {
+            cout << elemento << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    cout << ("OI\n");
+    ifstream arquivo = abrir_arquivo();
+    vector<vector<int>> matriz = ler_arquivo(arquivo);
+    
+    imprimir_matriz(matriz);
+    arquivo.close();
     return 0;
 }
